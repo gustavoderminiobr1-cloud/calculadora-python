aa7089e5d43daf75181d52b9da248073b538ad45
Gustavo Derminio <gustavoderminiobr1@gmail.com>
2026-07-23 13:26:32 -0300

    Adiciona testes para a função dividir

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,4 +1,6 @@
-from calculadora import somar, subtrair, multiplicar
+import pytest
+
+from calculadora import somar, subtrair, multiplicar, dividir
 
 
 def test_somar():
@@ -11,3 +13,12 @@ def test_subtrair():
 
 def test_multiplicar():
     assert multiplicar(4, 3) == 12
+
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+
+def test_dividir_por_zero():
+    with pytest.raises(ValueError):
+        dividir(10, 0)
